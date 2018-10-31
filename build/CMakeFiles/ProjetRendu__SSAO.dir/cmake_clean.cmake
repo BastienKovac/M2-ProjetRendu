0aef7ff5f1e693200db2df9d808af382ba723ed5
Bastien Kovac <bastien.kovac@gmail.com>
2018-10-31 17:05:34 +0100

    First commit

---
+++ build/CMakeFiles/ProjetRendu__SSAO.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetRendu__SSAO.dir/src/ProjetRendu/SSAO/SSAO.cpp.o"
+  "bin/ProjetRendu/ProjetRendu__SSAO.pdb"
+  "bin/ProjetRendu/ProjetRendu__SSAO"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjetRendu__SSAO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
